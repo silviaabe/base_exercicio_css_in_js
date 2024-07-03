a8980f6369a6bf12862b372ef9f36644f4108883
Silvia Abe <160606092+silviaabe@users.noreply.github.com>
2024-07-03 13:47:21 -0300

    Projeto CSS in JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import EstiloGlobal from './styles'
 
-import './global.css'
+import { Container } from './styles'
 
 function App() {
   return (
     <>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { ContainerHero, Form, HeroTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Form>
+    <ContainerHero>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </ContainerHero>
+  </Form>
 )
 
 export default Hero
